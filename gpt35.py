134a510cde6c7037114420016cd0f8c9a76b8431
Stephen Ng <stephen.ng@publicissapient.com>
2023-05-23 16:48:59 -0400

    Multiple users

--- gpt35.py
+++ gpt35.py
@@ -1,44 +1,78 @@
 import sys
 import json
 import openai
-from textData import importText
+from textData import importText, getCustomerInfo
 from secretKeys import OPEN_AI_KEY
 openai.api_key = OPEN_AI_KEY
 
 SYSTEM_SET = {
     "role": "system",
-    "content": "You are a sales person selling this hotel to the user.",
-    "role": "system",
-    "content": "Use the following the answer the queries: {}".format(importText(1))
+    "content": "You are a travel guide assisting the user.",
 }
 
-## KIOSK ROLE
-# "content": "You are a sales person selling this hotel experience to the user.",
+cName = "DEMO NAME"
+cTitle = "Mrs"
+cHotel = 0
+cId = 0
+
+
+def hotelInfo(customerHotel):
+    return {
+        "role": "system",
+        "content": "Use the following the answer the queries: {}".format(importText(customerHotel))
+    }
+
+
+def greetUser(customerName, customerTitle):
+    return {
+        "role": "system",
+        "content": "Preface the answer by greeting the user as {}.{}".format(customerTitle, customerName)
+    }
+
+# KIOSK ROLE
+# "content": "You are a travel guide assisting the user.",
 # "role": "system",
 # "content": "Use the following the answer the queries: {}".format(importText(1))
 
-## ASSISTANT ROLE
+# ASSISTANT ROLE
 # "content": "You are a helpful assistant.",
+
 
 def generatePrompt(prompt):
     return """{}""".format(prompt)
 
 
 def generateMessages(prompt, logContext):
+    global cName, cTitle, cHotel, cId
+
     messages = [SYSTEM_SET]
+    cId = sys.argv[2]
+    customerData = getCustomerInfo(cId)
+
+    try:
+        cName = customerData[1]
+        cTitle = customerData[2]
+        cHotel = customerData[3]
+        messages.append(hotelInfo(cHotel))
+    except:
+        return 0
 
     # Comment out if no context
-    with open('conversation-log.json', 'r') as log:
-        global context
-        context = json.load(log)
+    try:
+        with open('./conversations/{}.json'.format(cId), 'r') as log:
+            global context
+            context = json.load(log)
 
-    limitedContext = context[-logContext:]
+        limitedContext = context[-logContext:]
 
-    for line in limitedContext:
-        messages.append(line)
+        for line in limitedContext:
+            messages.append(line)
+
+    except FileNotFoundError:
+        context = []
+        messages.append(greetUser(cName, cTitle))
 
     requestMsg = {"role": "user", "content": generatePrompt(prompt)}
-
     messages.append(requestMsg)
     context.append(requestMsg)
 
@@ -46,6 +80,7 @@ def generateMessages(prompt, logContext):
 
 
 def request(prompt, logContext):
+    global cId
 
     response = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
@@ -57,9 +92,9 @@ def request(prompt, logContext):
     responseMsg = {"role": "assistant",
                    "content": response['choices'][0]['message']['content']}
     context.append(responseMsg)
-    with open("conversation-log.json", "w") as log:
+    with open("./conversations/{}.json".format(cId), "w") as log:
         json.dump(context, log)
 
 
-# Default voice assistant
+# Default voice assistant: 'python gpt35.py "Insert question here" CustomerID'
 request(sys.argv[1], 5)
--- textData.py
+++ textData.py
@@ -1,4 +1,5 @@
 import os
+import csv
 
 ## Import .txt files from ./data/ into a python object
 def importText(number):
@@ -6,3 +7,15 @@ def importText(number):
     text = f.read()
     f.close()
     return text
+
+## Import .csv files from ./data/stay.csv into a python object
+def getCustomerInfo(customerId):
+    f = csv.reader(open('data/stay.csv', "r"), delimiter=",")
+
+    #loop through the csv list
+    for customer in f:
+        #if current rows 2nd value is equal to input, print that row
+        if customerId == customer[0]:
+            return customer
+        
+    return 0
